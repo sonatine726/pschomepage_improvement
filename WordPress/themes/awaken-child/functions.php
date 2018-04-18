3faec1e2b987e7c396420de7efdd4f4346540beb
sonatine726 <sonatine726@gmail.com>
2018-04-18 18:07:27 +0900

    Revert functions.php to  the second last commit.

--- WordPress/themes/awaken-child/functions.php
+++ WordPress/themes/awaken-child/functions.php
@@ -2,14 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
-}
-
-add_action('wp_enqueue_scripts', 'enqueue_cherryblossom_js_in_only_toppage');
-function enqueue_cherryblossom_js_in_only_toppage() {
-    if (is_page('Home')) {
-        wp_register_script('cherryblossom_js', get_stylesheet_directory_uri()."/cherryblossom.js");
-        wp_enqueue_script('cherryblossom_js');
-    }
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style')
+);
 }
 ?>
